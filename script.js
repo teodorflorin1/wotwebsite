3ad03d0656744a34070415dd107ebc052d1b8f44
Teodor Florin Scornea <64328210+teodorflorin1@users.noreply.github.com>
2022-02-15 12:01:44 +0000

    update

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-function validate(){
-    var username = document.getElementById("emailAddress").value;
-    var password = document.getElementById("password").value;
-    
-    if(username=="admin" && password=="user")
-    {
-        window.location.href="index.html";
-        return false;
-    }
-    else{
-        alert("login failed");
-    }
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+const loginText = document.querySelector(".title-text .login");
+const signupText = document.querySelector(".title-text .signup");
 
-}
+
+
+signupBtn.onclick = (()=> {
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
+
+    
+
+});
+loginBtn.onclick = (()=> {
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
+
+});
+
+signupLink.onclick = (()=>{
+    signupBtn.click();
+    return false;
+});
+
+
